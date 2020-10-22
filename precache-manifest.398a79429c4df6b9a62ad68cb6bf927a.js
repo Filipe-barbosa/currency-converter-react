8095c12f582f8f4ba32e0e70fe5ea84801114008
unknown <filipebarbosa94@hotmail.com>
2020-10-21 22:21:23 -0300

    Updates

---
+++ precache-manifest.398a79429c4df6b9a62ad68cb6bf927a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec3988af867fc0ef275a6169c8ad060d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "15fb5e3861f5ba50be96",
+    "url": "/static/css/main.3a376305.chunk.css"
+  },
+  {
+    "revision": "27197fd3965ef728da68",
+    "url": "/static/js/2.4a635e07.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.4a635e07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15fb5e3861f5ba50be96",
+    "url": "/static/js/main.ac07722f.chunk.js"
+  },
+  {
+    "revision": "9611f592458c5446c45d",
+    "url": "/static/js/runtime-main.a4d7a8ea.js"
+  }
+]);
